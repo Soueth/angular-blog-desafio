1c20b097226bf67bfa1823a283e3706f1d7acaee
App Marketing/Dalan <suporte@appmarketing.com.br>
2024-01-11 16:22:33 -0300

    small-card maked

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { HeadCardComponent } from "../../Components/head-card/head-card.component";
 import { BigCardComponent } from "../../Components/big-card/big-card.component";
+import { SmallCardComponent } from "../../Components/small-card/small-card.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [HeadCardComponent, BigCardComponent]
+    imports: [HeadCardComponent, BigCardComponent, SmallCardComponent]
 })
 export class HomeComponent {
 
