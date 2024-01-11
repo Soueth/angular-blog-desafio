10f972ddcd468e0b98d842b74b64589e7871fb8d
App Marketing/Dalan <suporte@appmarketing.com.br>
2024-01-11 13:50:48 -0300

    head-card make

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeadCardComponent } from "./Components/head-card/head-card.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, HeadCardComponent]
 })
 export class AppComponent {
   title = 'angular-blog-desafio';
