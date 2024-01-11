9c396397f00bf818905bc83f7e253c6990085825
App Marketing/Dalan <suporte@appmarketing.com.br>
2024-01-11 17:27:24 -0300

    all components maked

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { HeadCardComponent } from "../../Components/head-card/head-card.component";
 import { BigCardComponent } from "../../Components/big-card/big-card.component";
 import { SmallCardComponent } from "../../Components/small-card/small-card.component";
+import { MenuComponent } from "../../Components/menu/menu.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [HeadCardComponent, BigCardComponent, SmallCardComponent]
+    imports: [HeadCardComponent, BigCardComponent, SmallCardComponent, MenuComponent]
 })
 export class HomeComponent {
 
