be37c7d0088c685762685b7ef152870fabb40841
App Marketing/Dalan <suporte@appmarketing.com.br>
2024-01-12 13:12:08 -0300

    menu are passed for AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeadCardComponent } from "./Components/head-card/head-card.component";
 import { HomeComponent } from "./Pages/home/home.component";
+import { MenuComponent } from "./Components/menu/menu.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, HeadCardComponent, HomeComponent]
+    imports: [CommonModule, RouterOutlet, HeadCardComponent, HomeComponent, MenuComponent]
 })
 export class AppComponent {
   title = 'angular-blog-desafio';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Pages/home/home.component';
+import { ArticleComponent } from './Pages/article/article.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'article',
+        component: ArticleComponent
+    }
+];
